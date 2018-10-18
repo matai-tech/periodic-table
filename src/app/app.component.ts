08f405b7973727bc846de6ffb68cbb71831e35e1
wenqi73 <1264578441@qq.com>
2018-10-18 18:50:32 +0800

    feat: add build.sh and rollup from ng-zorro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { MtaPeriodicTableService } from 'components/mta-periodic-table.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'periodic-table';
+
+  constructor(public service: MtaPeriodicTableService) {
+  }
 }
