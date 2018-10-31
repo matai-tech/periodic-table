11e1e717d89e85d6d87704037d8d9bde82df6b4b
张瑞淇 <zhangruiqi@mat.ai>
2018-10-31 10:50:06 +0800

    style:card hover

--- components/chemical-element/chemical-element.component.ts
+++ components/chemical-element/chemical-element.component.ts
@@ -42,14 +42,14 @@ import { MtaPeriodicTableService } from '../periodic-table.service';
     border: 1px solid transparent;
     border-radius: 2px;
     transition: box-shadow 0.28s cubic-bezier(0.4, 0, 0.2, 1);
-    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.14), 0 1px 5px 0 rgba(0, 0, 0, 0.12), 0 3px 1px -2px rgba(0, 0, 0, 0.2);
+    box-shadow: 0 2px 2px 0 rgba(0, 0, 0, 0.1), 0 1px 5px 0 rgba(0, 0, 0, 0.08), 0 3px 1px -2px rgba(0, 0, 0, 0.05);
   }
   .element-card.active,
   .element-card:hover {
-    border-color: #999;
+    border-color: #333;
     box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14),
       0 1px 10px 0 rgba(0, 0, 0, 0.12),
-      0 2px 4px -1px rgba(0, 0, 0, 0.4);
+      0 2px 4px -1px rgba(0, 0, 0, 0.6);
   }
   .alkali-metal {
     background-color: var(--alkali-metal-background-color);
